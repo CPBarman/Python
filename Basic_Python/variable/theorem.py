7a5a0dca4105688354937c5529866062ccbb08be
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-12 21:30:53 +0530

    first

--- Basic_Python/variable/theorem.py
+++ Basic_Python/variable/theorem.py
@@ -1,1 +1,30 @@
+#Variable_name= value
+
+
+number = 10
+print(type(number))
+
+
+x = 12
+print(type(x))
+
+
+x = 3.14
+print(type(x))
+
+
+name = "Jibon"
+print(type(name))
+
+
+num = 20
+print(num)
+
+
+x = 10
+print(x)
+
+
+nam = "Jibon"
+print(nam)
 
