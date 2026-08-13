7fa9a6e73c76179f9cd0f045bb02fa4517ff5624
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-13 18:13:17 +0530

    First commit

---
+++ Basic_Python/operator/relational_operator.py
@@ -0,0 +1,47 @@
+#------------------------------------------------------------------------------------------------#
+# Relational Operators
+# Operator       Name                            Example
+#   >            Greater Than                    a > b
+#   <            Less Than                       a < b
+#   >=           Greater Than or Equal To       a >= b
+#   <=           Less Than or Equal To          a <= b
+#   ==           Equal To                        a == b
+#   !=           Not Equal To                    a != b
+#
+#------------------------------------------------------------------------------------------------#
+
+# Example-1: [Greater Than Operator (>)]
+x = 3
+y = 2
+result = x > y
+print(result)
+
+# Example-1: [Less Than Operator (<)]
+x = 4
+y = 3
+result = x > y
+print(result)
+
+# Example-3: [Greater Than or Equal To Operator (>=)]
+x = 2
+y = 2
+result = x >= y
+print(result)
+
+# Example-4: [Less Than or Equal To Operator (<=)]
+x = 2
+y = 3
+result = x <= y
+print(result)
+
+# Example-5: [Equal To Operator (==)]
+x = 4
+y = 4
+result = x == y
+print(result)
+
+# Example-5: [Not Equal To Operator (==)]
+x = 2
+y = 4
+result = x != y
+print(result)
