2032218c28f1d9a54f2c35436136712a36e6a049
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-13 13:00:17 +0530

    First commit

--- Conditional_Statements/if_elif_else_statement/theorem.py
+++ Conditional_Statements/if_elif_else_statement/theorem.py
@@ -1,1 +1,51 @@
+#------------------------------------------------------------------------------------------------#
+# Structure-1: Basic Syntax of if statement
+# if condition:
+#    statement
+# elif condition:
+#    statement
+# else:
+#    statement  
+#------------------------------------------------------------------------------------------------#
 
+# Example-1
+age = 20
+if age >= 18:
+    print("You are adult.")
+elif age >=13:
+    print("You are teenager.")
+else:
+    print("You are child.")
+
+
+
+#------------------------------------------------------------------------------------------------#
+# Structure-2: if Statement with User Input
+# user_input = type(input("Input Name: "))
+# if user_input condition:
+#    statement
+# elif user_input condition:
+#    statement
+# else user_input condition:
+#    statement
+#------------------------------------------------------------------------------------------------#
+
+#Example-1
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are adalt.")
+elif age >= 13:
+    print("You are teenager.")
+else:
+    print("You are child.")
+
+#Example-2
+mark = int(input("Enter your mark: "))
+if mark >= 80:
+    print("You got A+")
+elif mark >= 70:
+    print("You got A")
+elif mark >= 60:
+    print("You Passed")
+else:
+    print("You failed")
