1a8d95059afee4f1b75b84b073c5558f571c517c
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-13 17:49:23 +0530

    First commit

--- Basic_Python/operator/arithmetic_operator.py
+++ Basic_Python/operator/arithmetic_operator.py
@@ -1,1 +1,55 @@
+#------------------------------------------------------------------------------------------------#
+# Arithmetic Operators
+# Operator       Name                    Example
+#   +            Addition                a + b
+#   -            Subtraction             a - b
+#   *            Multiplication          a * b
+#   /            Division                a / b
+#   //           Floor Division           a // b
+#   %            Modulus (Remainder)     a % b
+#   **           Exponentiation          a ** b
+#------------------------------------------------------------------------------------------------#
+
+#Example-1 [Addition]
+x = 2
+y = 3
+result = x + y
+print(result)
+
+#Example-2 [Subtraction]
+x = 4
+y = 6
+result = y - x
+print(result)
+
+#Example-3 [ Multiplication]
+x = 3
+y = 2
+result = x * y
+print(result)
+
+#Example-4 [Division]
+x = 6
+y = 2
+result = x / y
+print(result)
+
+#Example-5 [Floor Division ]
+x = 10
+y = 2
+result = x // y
+print(result)
+
+#Example-6 [Modulus (Remainder)]
+x = 8
+y = 6
+result = x % y
+print(result)
+
+#Example-7 [Exponentiation]
+x = 3
+y = 2
+result = x ** y
+print(result)
+
 
