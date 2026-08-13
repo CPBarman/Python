6aab6ff2bc4d32033a9d9463bce4de08782ae7b6
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-13 18:29:19 +0530

    First Commit

--- Basic_Python/variable/theorem.py
+++ Basic_Python/variable/theorem.py
@@ -1,34 +1,37 @@
-#Variable_name= value
+#------------------------------------------------------------------------------------------------#
+# Structure: 
+# Variable_name= value
+#------------------------------------------------------------------------------------------------#
 
-
+#Example-1
 number = 10
 print(type(number))
 
-
+#Example-2
 x = 12
 print(type(x))
 
-
+#Example-3
 x = 3.14
 print(type(x))
 
-
+#Example-4
 name = "Jibon"
 print(type(name))
 
-
+#Example-5
 num = 20
 print(num)
 
-
+#Example-6
 x = 10
 print(x)
 
-
+#Example-7
 nam = "Jibon"
 print(nam)
 
-
+#Example-7
 sl = 1
 name = "Roy"
 age = 23
