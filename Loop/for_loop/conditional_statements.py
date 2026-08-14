65317956146f40c904ae66baa8df7356ba3ca0b4
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-14 15:12:51 +0530

    First

---
+++ Loop/for_loop/conditional_statements.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------------------------#
+# Structure-6: for Loop with if Condition
+# for <variable> in <iterable>:
+#     if <condition>:
+#         <statement>
+#------------------------------------------------------------------------------------------------#
+
+#Example-1
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+
+#Example-2
+numbers = [10, 25, 60, 45, 80, 30, 90]
+for number in numbers:
+    if number > 50:
+        print(number)
+        
