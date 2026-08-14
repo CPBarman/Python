6e18e5efba5f1a205a6e3e7a93ebcaa25ec4188b
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-08-14 08:30:22 +0530

    First commit

--- Loop/for_loop/Theorem.py
+++ Loop/for_loop/Theorem.py
@@ -1,1 +1,47 @@
+#------------------------------------------------------------------------------------------------#
+# Structure-1: for loop with range(stop)
+# for variable in range(value):
+#    <statement>
+#------------------------------------------------------------------------------------------------#
 
+#Example-1
+for i in range(5):
+    print("Hello")
+
+#Example-2
+for i in range(5):
+    print(i)
+
+#Example-3
+for i in range(10):
+    print(i)
+
+
+#------------------------------------------------------------------------------------------------#
+# Structure-2: for loop with range(start, stop)
+# for variable in range range(start, stop):
+#    <statement>
+#------------------------------------------------------------------------------------------------#
+
+#Example-1
+for i in range(1,6):
+    print(i)
+
+#Example-2
+for i in range(2,6):
+    print(i)
+
+
+#------------------------------------------------------------------------------------------------#
+#  Structure-3: for loop with range(start, stop, step)
+# for variable in range range(start, stop):
+#    <statement>
+#------------------------------------------------------------------------------------------------#
+
+#Example-1
+for i in range(2,6,1):
+    print(i)
+
+#Example-2
+for i in range(2,6,3):
+    print(i)
